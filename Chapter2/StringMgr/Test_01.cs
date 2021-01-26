d4a774c2fe39fdcca54b2661fcc10410cce3a264
王悦 <yue.wang@puzzlecastle.co>
2021-01-26 15:21:50 +0800

    2021.1.26
    上传基本文件

---
+++ Chapter2/StringMgr/Test_01.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace StringMgr
+{
+    public class Test_01
+    {
+        public static void Test()
+        {
+            var myString = "my \"so-called\" life";
+            Console.WriteLine(myString);
+        }
+
+    }
+}
