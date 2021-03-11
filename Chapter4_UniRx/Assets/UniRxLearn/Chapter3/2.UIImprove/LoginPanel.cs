dcbc35c7417de559a87cddec26fa44ab1d9b1761
王悦 <yue.wang@puzzlecastle.co>
2021-03-11 10:24:47 +0800

    UniRx 工程更新

--- Chapter4_UniRx/Assets/UniRxLearn/Chapter3/2.UIImprove/LoginPanel.cs
+++ Chapter4_UniRx/Assets/UniRxLearn/Chapter3/2.UIImprove/LoginPanel.cs
@@ -10,10 +10,11 @@ public class LoginPanel: MonoBehaviour
     [SerializeField] private InputField _inputName;
     [SerializeField] private InputField _inputPassword;
     [SerializeField] private Button _btnLogin;
-
+    [SerializeField] private Button _btnRegister;
+    
     private string _myName;
     private string _myPassword;
-    
+
     private void Start()
     {
         _btnLogin.OnClickAsObservable().Subscribe(_ =>
@@ -22,7 +23,17 @@ public class LoginPanel: MonoBehaviour
             Debug.Log($"myName: {_myName} \nmyPassword {_myPassword}");
         });
 
-        _inputName.OnEndEditAsObservable().Subscribe(_ => _myName = _);
-        _inputPassword.OnEndEditAsObservable().Subscribe(_ => _myPassword = _);
+        _inputName.OnEndEditAsObservable().Subscribe(_ =>
+        {
+            _myName = _;
+            _inputPassword.Select();
+        });
+        _inputPassword.OnEndEditAsObservable().Subscribe(_ =>
+        {
+            _myPassword = _;
+            _btnLogin.onClick.Invoke();
+        });
+
+        _btnRegister.OnClickAsObservable().Subscribe(_ => { });
     }
 }
---
+++ Chapter4_UniRx/Assets/UniRxLearn/Chapter3/2.UIImprove/RegisterPanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterPanel : MonoBehaviour
+{
+    [SerializeField] private InputField _inputName;
+    [SerializeField] private InputField _inputPass1;
+    [SerializeField] private InputField _inputPass2;
+    [SerializeField] private Button _btnRegister;
+
+    private string _name;
+    private string _password;
+
+    private void Start()
+    {
+        _inputName.OnEndEditAsObservable().Subscribe(result =>
+        {
+            _inputPass1.Select();
+        });
+
+        _inputPass1.OnEndEditAsObservable().Subscribe(result =>
+        {
+            _inputPass2.Select();
+        });
+
+        _inputPass2.OnEndEditAsObservable().Subscribe(result =>
+        {
+            _btnRegister.onClick.Invoke();
+        });
+    }
+}
